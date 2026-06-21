28685ff5e2659b468cf7637ea1004266170079a4
Julian Braha <julianbraha@gmail.com>
2026-06-21 20:14:16 +0100

    kconfirm-desugar: fix expand_source (may be inside menu, choice, if)

--- kconfirm-desugar/src/expand_source.rs
+++ kconfirm-desugar/src/expand_source.rs
@@ -15,6 +15,24 @@ pub fn visit_source(source: Source) -> Vec<Entry> {
 pub fn visit_entry(entry: Entry) -> Vec<Entry> {
     return match entry {
         Entry::Source(source) => visit_source(source),
+        // `source` can also appear inside these containers, so recurse to expand
+        // any sources nested within them.
+        Entry::If(mut r#if) => {
+            r#if.entries = visit_entries(r#if.entries);
+            vec![Entry::If(r#if)]
+        }
+        Entry::Menu(mut menu) => {
+            menu.entries = visit_entries(menu.entries);
+            vec![Entry::Menu(menu)]
+        }
+        Entry::Choice(mut choice) => {
+            choice.entries = visit_entries(choice.entries);
+            vec![Entry::Choice(choice)]
+        }
         _ => vec![entry],
     };
+}
+
+fn visit_entries(entries: Vec<Entry>) -> Vec<Entry> {
+    entries.into_iter().flat_map(visit_entry).collect()
 }
